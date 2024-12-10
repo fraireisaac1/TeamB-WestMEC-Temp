0bf006d97e5d70d3a355d592166392e36cd2cadf
Myles Marshall <mmarsh319@west-mec.org>
2024-12-09 17:26:41 -0700

    login page file

---
+++ vite-project/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+// login page structure placeholder
+
+export default function Login() {
+    return (
+        <>
+            <div className="login-form">
+                <form id="login">
+
+                </form>
+            </div>
+        </>
+    )
+}
